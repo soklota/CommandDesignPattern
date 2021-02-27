ec1cdf69340ba5547e27ac901b05f91a9951b6bf
Jackson Carroll <Jack Carroll>
2021-02-27 14:02:46 -0500

    added definitions and comments

--- HealCommand.java
+++ HealCommand.java
@@ -1,12 +1,26 @@
+/**
+ * Heal command class implementing Command class
+ * @author Overachievers
+ */
 public class HealCommand implements Command{
+    private Robot robot;
 
+    /**
+     * Constructor for heal command setting robot to local robot
+     * @param robot A Robot for the robot
+     */
     public HealCommand(Robot robot){
 
+        this.robot = robot;
 
     }
 
+    /**
+     * Executes the heal command 
+     */
     public void execute(){
 
+        robot.heal();
 
     }
 
