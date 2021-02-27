873b48f8d8cc3da5897ff55b358409585266228e
Jackson Carroll <Jack Carroll>
2021-02-27 13:59:05 -0500

    added definitions and comments

--- JumpCommand.java
+++ JumpCommand.java
@@ -1,10 +1,26 @@
+/**
+ * Jump command class implementing Command class
+ * @author Overachievers
+ */
 public class JumpCommand implements Command{
+    private Robot robot;
 
+    /**
+     * Constructor for jump command setting given to local robot 
+     * @param robot
+     */
     public JumpCommand(Robot robot){
+
+        this.robot = robot;
 
     }
 
+    /**
+     * Executes the jump command
+     */
     public void execute(){
+
+        robot.jump();
 
     }
     
