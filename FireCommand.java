ab215c519d9315a36c5d5d79b4b0ef9e8a3ebfe7
Jackson Carroll <Jack Carroll>
2021-02-27 13:50:09 -0500

    added definitions and comments

--- FireCommand.java
+++ FireCommand.java
@@ -1,11 +1,23 @@
+/**
+ * Fire command class implementing Command class
+ * @author Overachievers
+ */
 public class FireCommand implements Command{
-    
-    public FireCommand(Robot robot){
+    private Robot robot;
 
+    /**
+     * Constructor to set given robot to local robot
+     * @param robot A Robot for robot
+     */
+    public FireCommand(Robot robot){
+        this.robot = robot;
     }
 
+    /**
+     * Executes the pickup command
+     */
     public void execute(){
-
+        robot.pickup();
     }
 
 
