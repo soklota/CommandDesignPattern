e79fa23798325932ad11720838ee0db8029e6fef
rosesatriano <satrianr@email.sc.edu>
2021-02-27 18:37:01 -0500

    formatting

--- Robot.java
+++ Robot.java
@@ -3,51 +3,39 @@
  * @author Overachievers
  */
 public class Robot {
-
     /**
      * Constructor for Robot Class
      * @param name String for the name of the robot
      */
     Robot(String name){
-
         System.out.println("Welcome, my name is " + name + ", I'm a friendly robot!");
-
     }
 
     /**
      * Pickup command for the robot to perform
      */
     public void pickup(){
-
         System.out.println("Oh treasure, picking it up!");
-
     }
 
     /**
      * Jump command for the robot to perform
      */
     public void jump(){
-
         System.out.println("Oh no a hazard, I'm jumping over it.");
-
     }
 
     /**
      * Fire command for the robot to perform
      */
     public void fire(){
-
         System.out.println("Bad guys! Fire my blow torch at them.");
-
     }
 
     /**
      * Heal command for the robot to perform
      */
     public void heal(){
-
         System.out.println("Thanks for healing my wounds.");
-        
     }
-    
 }
